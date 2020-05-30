ab5077a9793ddb80cf580b985084f66e733eec96
Wcapers <Mirazizovt@yandex.ru>
2020-05-31 01:23:36 +0300

    add methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def get_tests (level)
+    Test.joins('join results on tests.id = results.test_id').where(
+      'results.user_id = ? and tests.level = ?', self.id, level)
+  end
 end
