0c95b41cbd5d42f07751376b2d41e07eb2f337b8
Wcapers <Mirazizovt@yandex.ru>
2020-06-05 21:54:36 +0300

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_180716) do
     t.boolean "correct", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "questions_id", null: false
-    t.index ["questions_id"], name: "index_answers_on_questions_id"
+    t.integer "question_id", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -31,17 +31,8 @@ ActiveRecord::Schema.define(version: 2020_06_05_180716) do
     t.text "body", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "tests_id"
-    t.index ["tests_id"], name: "index_questions_on_tests_id"
-  end
-
-  create_table "results", force: :cascade do |t|
     t.integer "test_id"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_results_on_test_id"
-    t.index ["user_id"], name: "index_results_on_user_id"
+    t.index ["test_id"], name: "index_questions_on_test_id"
   end
 
   create_table "tests", force: :cascade do |t|
@@ -49,9 +40,9 @@ ActiveRecord::Schema.define(version: 2020_06_05_180716) do
     t.integer "level", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "categories_id", null: false
+    t.integer "category_id", null: false
     t.integer "author_id", default: 1, null: false
-    t.index ["categories_id"], name: "index_tests_on_categories_id"
+    t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
   create_table "tests_users", id: false, force: :cascade do |t|
@@ -69,9 +60,7 @@ ActiveRecord::Schema.define(version: 2020_06_05_180716) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "answers", "questions", column: "questions_id"
-  add_foreign_key "questions", "tests", column: "tests_id"
-  add_foreign_key "results", "tests"
-  add_foreign_key "results", "users"
-  add_foreign_key "tests", "categories", column: "categories_id"
+  add_foreign_key "answers", "questions"
+  add_foreign_key "questions", "tests"
+  add_foreign_key "tests", "categories"
 end
