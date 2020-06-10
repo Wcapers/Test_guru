4366df44244753bf2222fa4eea2d0994b19aded7
Wcapers <Mirazizovt@yandex.ru>
2020-06-10 19:02:24 +0300

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users
   has_many :tests_author, class_name: 'Test', foreign_key: :author_id
   def get_tests (level)
-    self.tests.where("level = ?", level)
+    self.tests.where(level: level)
   end
 end
