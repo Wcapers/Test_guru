3f9562809ca3ee4f3e47e794eee725a9ba5759d7
Wcapers <Mirazizovt@yandex.ru>
2020-06-14 16:46:42 +0300

    Add scopes

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,8 +5,11 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
+  scope :easy_tests, -> { where(level: 0..1) }
+  scope :normal_tests, -> { where(level: 2..4) }
+  scope :hard_tests, -> { where(level: 5..Float::INFINITY) }
   def self.get_titles(category_name)
-     Test.joins('join categories on categories.id = tests.categories_id')
-     .where('categories.title = ?', category_name).order(title: :desc).pluck(:title)
+    Test.joins('join categories on categories.id = tests.categories_id').where(
+      'categories.title = ?', category_name).order(title: :desc).pluck(:title)
    end
 end
