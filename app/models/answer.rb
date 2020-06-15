2180287d9f672fb2fba1a041134e65400e4b8de1
Wcapers <Mirazizovt@yandex.ru>
2020-06-15 18:41:59 +0300

    add validates

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,10 @@
 class Answer < ApplicationRecord
+  validates :body, presence: true
   belongs_to :question
-   scope :corrects, -> { where(correct: true) }
-
+  scope :corrects, -> { where(correct: true) }
+  validates :validate_answers
+  private
+  def validate_answers
+    errors.add(:question) if self.question.answers.count > 4
+  end
 end
--- app/models/test.rb
+++ app/models/test.rb
@@ -4,10 +4,13 @@ class Test < ApplicationRecord
   has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
-
+  validates :title, presence: true
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :level, uniqueness: { scope: :title, message: "Может существовать только один Тест с данным названием и уровнем" }
   scope :easy_tests, -> { where(level: 0..1) }
   scope :normal_tests, -> { where(level: 2..4) }
   scope :hard_tests, -> { where(level: 5..Float::INFINITY) }
+
   def self.get_titles(category_name)
     Test.joins('join categories on categories.id = tests.categories_id').where(
       'categories.title = ?', category_name).order(title: :desc).pluck(:title)
