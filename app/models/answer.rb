d10fa6b42ae4ce4b11a9dd5341578b4888534fc8
Wcapers <Mirazizovt@yandex.ru>
2020-06-15 18:48:35 +0300

    fix

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,7 +2,7 @@ class Answer < ApplicationRecord
   validates :body, presence: true
   belongs_to :question
   scope :corrects, -> { where(correct: true) }
-  validates :validate_answers
+  validate :validate_answers
   private
   def validate_answers
     errors.add(:question) if self.question.answers.count > 4
