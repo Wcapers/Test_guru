68e0c45b5ca11e9c9d560a2cda7a57a1fe22c2b9
Wcapers <Mirazizovt@yandex.ru>
2020-07-03 23:06:58 +0300

    add index page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,13 @@
 class QuestionsController < ApplicationController
+  before_action :find_test
+
+  def index
+    @questions = @test.questions
+  end
+
+  private
+
+  def find_test
+    @test = Test.find params[:test_id]
+  end
 end
