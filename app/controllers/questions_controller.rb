57b81b9a3f18478c3b34f1031a9f89c355036564
Wcapers <Mirazizovt@yandex.ru>
2020-07-03 23:32:23 +0300

    add show method

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,22 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: %i[index]
+  before_action :find_question, only: %i[show]
 
   def index
     @questions = @test.questions
+  end
+
+  def show
+    render inline: "<h1><%=@question.body%><\h1>"
   end
 
   private
 
   def find_test
     @test = Test.find params[:test_id]
+  end
+
+  def find_question
+    @question = Question.find params[:id]
   end
 end
