e1e8c3a84d1a05f29567a998e7f50335edeecd58
Wcapers <Mirazizovt@yandex.ru>
2020-07-26 20:47:09 +0300

    nav complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to(repo, "https://github.com/#{author}/#{repo}", target: "_blank")
   end
+
+  def flash_messages(flash)
+   flash.map do |key, msg|
+     content_tag :div, msg, id: key, class: 'flash alert'
+   end.join.html_safe
+ end
 end
