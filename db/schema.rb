a3a89335186fc5093e0c70880b99df6dd847206c
Wcapers <Mirazizovt@yandex.ru>
2020-07-28 15:25:46 +0300

    after review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_24_154345) do
+ActiveRecord::Schema.define(version: 2020_07_28_122326) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
