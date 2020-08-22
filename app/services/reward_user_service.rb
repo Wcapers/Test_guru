3e978f1cea5964788921587b95c44e36803b4562
Wcapers <Mirazizovt@yandex.ru>
2020-08-22 21:34:03 +0300

    after review

--- app/services/reward_user_service.rb
+++ app/services/reward_user_service.rb
@@ -15,18 +15,18 @@ class RewardUserService
 
   def all_level_tests_complete? (level)
     level = level.to_i
-    @test_passage.test.level == level && @test_passage.success? &&
-     @user.tests.where("level = ?", level).uniq.count == Test.where("level = ?", level).count
+    @test_passage.test.level == level && @user.tests.where("level = ?", level).uniq.count ==
+     Test.where("level = ?", level).count
   end
 
   def all_category_tests_complete? (category_id)
     category = category_id.to_i
-    @test_passage.test.category_id == category && @test_passage.success? &&
-     @user.tests.where("category_id = ?", category).uniq.count == Test.where("category_id = ?", category).count
+    @test_passage.test.category_id == category && @user.tests.where("category_id = ?", category).uniq.count ==
+     Test.where("category_id = ?", category).count
   end
 
   def first_try_complete? (title)
-     @test_passage.test.title == title && @test_passage.success? && @user.tests.where("title = ?", @test_passage.test.title).count == 1
+     @test_passage.test.title == title && @user.tests.where("title = ?", @test_passage.test.title).count == 1
   end
 
   def reward(badge)
